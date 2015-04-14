8b5b9a19933cd2cfb1eb7029903ae585b94c24c0
lbj96347 <cashlee96347@gmail.com>
2015-04-14 15:30:25 +0800

    [mod]rename convert filename

--- ConvertCore/ChineseConvert.m
+++ ConvertCore/ChineseConvert.m
@@ -5,7 +5,7 @@
 //  Copyright 2015 cashlee.info. All rights reserved.
 //
 
-#import "convertGB_BIG.h"
+#import "ChineseConvert.h"
 
 @implementation ConvertGB_BIG
 @synthesize string_GB = _string_GB;
